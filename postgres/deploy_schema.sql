5e3881e95097e2e236d72f09e8f75b76b7822d74
Onix <gotnix13@gmail.com>
2022-05-12 22:48:24 -0400

    Added seed data and dockerization for webapp

--- postgres/deploy_schema.sql
+++ postgres/deploy_schema.sql
@@ -1,3 +1,6 @@
 -- Deploy fresh DB tables. \i executes sql scripts
 \i '/docker-entrypoint-initdb.d/tables/users.sql'
 \i '/docker-entrypoint-initdb.d/tables/login.sql'
+
+-- _ Add seed data
+\i '/docker-entrypoint-initdb.d/seed/seed.sql'
---
+++ postgres/seed/seed.sql
@@ -0,0 +1,9 @@
+begin transaction;
+insert into users (name, email, entries, joined)
+values ('Puto', 'p@email.com',0,'2022-05-13 02:18:32.417');
+commit;
+
+begin transaction;
+insert into login (hash, email)
+values ('$2a$10$Hgnv5MWhKKViwyT6mk.7XOTyaeJ9ijyDODAOixElI4BlYPM9Eg91u','jojo@email.com');
+commit;
